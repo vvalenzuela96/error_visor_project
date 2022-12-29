fe801adba08abcdc909bd582a6e5ad4ba42560b5
Victor Valenzuela M <vvalenzuela@itsanet>
2022-12-29 11:38:39 -0300

    Function Logger.err() added _to_dict() para parsear Error or Warning en un dict json

--- error_visor/logger.py
+++ error_visor/logger.py
@@ -4,8 +4,13 @@
 """
 
 import json
+from inspect import currentframe, getframeinfo
 from datetime import datetime
 from enum import Enum
+from types import FrameType
+
+
+date_format = "%d/%m/%Y, %H:%M:%S"
 
 class Priority(Enum):
     LOW = 'low'
@@ -13,29 +18,71 @@ class Priority(Enum):
     HIGH = 'high'
 
 class Error():
+    'Para acciones que generan problema'
     description:str
     timestamp:datetime
     priority:Priority
+    ex:Exception
 
     def __init__(
             self,
-            description:str,
-            timestamp:datetime,
-            priority:Priority) -> None:
+            description:str='',
+            timestamp:datetime=datetime.now(),
+            priority:Priority=Priority.MID,
+            ex:Exception=None) -> None:
         self.description = description
         self.timestamp = timestamp
         self.priority = priority
+        self.ex = ex
     
-    def log():
-        pass
+    def __repr__(self) -> str:
+        return f"""Error(
+                        description={self.description},
+                        timestamp={self.timestamp},
+                        priority={self.priority}
+                        ex={self.ex}
+                    )
+                    """
 
 class Warning():
-    description:str
-    timestamp:datetime
-    
+    'Para acciones que podrian generar problemas, pero funcionan igual'
+    #TODO Warning class code
+
+def _to_dict(obj:Error | Warning, frame:FrameType) -> dict:
+    tb = getframeinfo(frame)
+    if isinstance(obj, Error):
+        return {
+            'type': 'error',
+            'description': obj.description,
+            'timestamp': obj.timestamp.strftime(date_format),
+            'priority': obj.priority.value,
+            'ex_args': obj.ex.args,
+            'ex_doc': obj.ex.__doc__,
+            'line': tb.lineno,
+            'filename': tb.filename.split('\\')[-1],
+            'class': frame.f_code.co_qualname,
+            'function': tb.function,
+            'positions': tb.positions
+        }
+    elif isinstance(obj, Warning):
+        pass #TODO Warning transformation to dict
 
 class Logger():
     @staticmethod
-    def deposit():
-        pass
+    def err(error:Error, printable:bool=False) -> bool:
+        frame = currentframe().f_back
+        error_dict  = _to_dict(error, frame)
 
+        try:
+            with open('logs/log.ev', 'x') as file:
+                file.write('[]')
+        except FileExistsError as ex:
+            pass
+
+        with open('logs/log.ev', 'r') as file:
+            lista:list = json.load(file)
+        
+        lista.append(error_dict)
+
+        with open('logs/log.ev', 'w') as file:
+            file.write(json.dumps(lista))
--- test.py
+++ test.py
@@ -1,13 +1,43 @@
 
 import error_visor as EV
 
-EV.Logger.deposit()
-
 from inspect import currentframe, getframeinfo
 
 def info():
-    return getframeinfo(currentframe().f_back)
+    trak = getframeinfo(currentframe().f_back)
+    return trak
 
 
 frame_info = info()
-print(frame_info)
+print(frame_info.lineno)
+
+class TestLogger:
+    def testing_logger():
+        try:
+            print('Test de excepcion repr')
+            with open('a'):
+                pass
+        except FileNotFoundError as ex:
+            print(ex.args)
+            print('###################################')
+            print(ex.__cause__)
+            print('###################################')
+            print(ex.__context__)
+            print('###################################')
+            print(ex.__dict__)
+            print('###################################')
+            print(ex.__doc__)
+            EV.Logger.err(EV.Error(description='Error de prueba', ex=ex, priority=EV.Priority.HIGH))
+            print('###################################')
+            # print(ex.__annotations__)
+            # print('###################################')
+            # print(ex.__annotations__)
+            # print('###################################')
+            # print(ex.__annotations__)
+            # print('###################################')
+            # print(ex.__annotations__)
+            # print('###################################')
+            # print(ex.__annotations__)
+
+
+TestLogger.testing_logger()
